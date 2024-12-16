6afca28e57117ce894948d87259e65922b7af32a
Ture Bentzin <ture.bentzin@alumni.fh-aachen.de>
2024-12-16 14:35:22 +0100

    initial commit

---
+++ src/main/java/de/bentzin/reke/Main.java
@@ -0,0 +1,12 @@
+package de.bentzin.reke;
+
+
+/**
+ * @author Ture Bentzin
+ * @since 16-12-2024
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
